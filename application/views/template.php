d350991569043bb3c6fd2966c5c374c60801697b
Ian Luckraft <ianckc@mba.ac.uk>
2011-06-10 15:27:07 +0100

    Altered the css and javascript paths in the template to match my assets/ configuration. Copied across used css and javascript

--- application/views/template.php
+++ application/views/template.php
@@ -26,37 +26,38 @@ URL: http://stuffandnonsense.co.uk/projects/320andup
 <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1">
 
 <!-- For less capable mobile browsers
-<link rel="stylesheet" media="handheld" href="css/handheld.css?v=1">  -->
+<link rel="stylesheet" media="handheld" href="assets/css/handheld.css?v=1">  -->
 
 <!-- For all browsers -->
-<link rel="stylesheet" media="screen" href="css/style.css?v=1">
-<link rel="stylesheet" media="print" href="css/print.css?v=1">
+<link rel="stylesheet" media="screen" href="assets/css/style.css?v=1">
+<link rel="stylesheet" media="screen" href="assets/css/forms.css?v=1">
+<link rel="stylesheet" media="print" href="assets/css/print.css?v=1">
 <!-- For progressively larger displays -->
-<link rel="stylesheet" media="only screen and (min-width: 480px)" href="css/480.css?v=1">
-<link rel="stylesheet" media="only screen and (min-width: 768px)" href="css/768.css?v=1">
-<link rel="stylesheet" media="only screen and (min-width: 992px)" href="css/992.css?v=1">
-<link rel="stylesheet" media="only screen and (min-width: 1382px)" href="css/1382.css?v=1">
+<link rel="stylesheet" media="only screen and (min-width: 480px)" href="assets/css/480.css?v=1">
+<link rel="stylesheet" media="only screen and (min-width: 768px)" href="assets/css/768.css?v=1">
+<link rel="stylesheet" media="only screen and (min-width: 992px)" href="assets/css/992.css?v=1">
+<link rel="stylesheet" media="only screen and (min-width: 1382px)" href="assets/css/1382.css?v=1">
 <!-- For Retina displays -->
-<link rel="stylesheet" media="only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)" href="css/2x.css?v=1">
+<link rel="stylesheet" media="only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)" href="assets/css/2x.css?v=1">
 
 <!-- JavaScript at bottom except for Modernizr -->
-<script src="js/libs/modernizr-1.7.min.js"></script>
+<script src="assets/javascript/libs/modernizr-1.7.min.js"></script>
 
 <!-- For iPhone 4 -->
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/h/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/h/apple-touch-icon.png">
 <!-- For iPad 1-->
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/m/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/m/apple-touch-icon.png">
 <!-- For iPhone 3G, iPod Touch and Android -->
-<link rel="apple-touch-icon-precomposed" href="img/l/apple-touch-icon-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/l/apple-touch-icon-precomposed.png">
 <!-- For Nokia -->
-<link rel="shortcut icon" href="img/l/apple-touch-icon.png">
+<link rel="shortcut icon" href="assets/images/l/apple-touch-icon.png">
 <!-- For everything else -->
 <link rel="shortcut icon" href="/favicon.ico">
 
 <!--iOS. Delete if not required -->
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<link rel="apple-touch-startup-image" href="img/splash.png">
+<link rel="apple-touch-startup-image" href="assets/images/splash.png">
 
 <!--Microsoft. Delete if not required -->
 <meta http-equiv="cleartype" content="on">
@@ -87,15 +88,15 @@ URL: http://stuffandnonsense.co.uk/projects/320andup
 
 <!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
-<script>window.jQuery || document.write('<script src="js/libs/jquery-1.5.1.min.js">\x3C/script>')</script>
+<script>window.jQuery || document.write('<script src="assets/javascript/libs/jquery-1.5.1.min.js">\x3C/script>')</script>
 <!-- Scripts -->
-<script src="js/plugins.js"></script>
-<script src="js/script.js"></script>
+<script src="assets/javascript/plugins.js"></script>
+<script src="assets/javascript/script.js"></script>
 
 <!--[if (lt IE 9) & (!IEMobile)]>
-<script src="js/libs/DOMAssistantCompressed-2.8.js"></script>
-<script src="js/libs/selectivizr-1.0.1.js"></script>
-<script src="js/libs/respond.min.js"></script>
+<script src="assets/javascript/libs/DOMAssistantCompressed-2.8.js"></script>
+<script src="assets/javascript/libs/selectivizr-1.0.1.js"></script>
+<script src="assets/javascript/libs/respond.min.js"></script>
 <![endif]-->
 
 <!-- http://t.co/HZe9oJ4 -->
